3527fb18a98b5d32863c796aa7d4bc3379942879
Oleksii Fedorenko <alex4rest1@gmail.com>
2022-08-08 21:33:18 +0300

    added auto move for snake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import time
 
 class Snake:
     def __init__(self, parent_screen):
@@ -7,27 +8,37 @@ class Snake:
         self.block = pygame.image.load("resources/stone-zhang-icon.png").convert()
         self.x = 100
         self.y = 100
+        self.direction = 'down'
 
     def draw(self):
         self.parent_screen.fill((102, 2, 0))
         self.parent_screen.blit(self.block, (self.x, self.y))
         pygame.display.flip()
+
+    def walk(self):
+        if self.direction == 'left':
+            self.x -= 10
+        if self.direction == 'right':
+            self.x += 10
+        if self.direction == 'up':
+            self.y -= 10
+        if self.direction == 'down':
+            self.y += 10
+
+        self.draw()
     
     def move_left(self):
-        self.x -= 10
-        self.draw()
+        self.direction = 'left'
 
     def move_right(self):
-        self.x += 10
-        self.draw()
+        self.direction = 'right'
 
     def move_up(self):
-        self.y -= 10
-        self.draw()
+        self.direction = 'up'
 
     def move_down(self):
-        self.y += 10
-        self.draw()
+        self.direction = 'down'
+
 
 class Game:
     def __init__(self):
@@ -39,6 +50,7 @@ class Game:
 
     def run(self):
         running = True
+
         while running:
             for event in pygame.event.get():
                 if event.type == KEYDOWN:
@@ -59,6 +71,10 @@ class Game:
 
                 elif event.type == QUIT:
                     running = False
+
+            self.snake.walk()
+            time.sleep(0.2)
+
 
 if __name__ == "__main__":
     game = Game()
